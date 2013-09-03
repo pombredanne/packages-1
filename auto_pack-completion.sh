dc6152503c24f74cbaa7cfebfb931e2285718a92
Oleg Ryabukhin <oleg@MacBook-Air-Oleg-2.local>
2013-09-03 18:00:40 +0400

    bash autocompletion for gogo projects

--- auto_pack-completion.sh
+++ auto_pack-completion.sh
@@ -6,5 +6,11 @@ _auto_pack()
     COMPREPLY=( $(compgen -W "$(cat ~/packages/data/all_gogo_projects)" -- $cur) )
 }
 
+_make_go_project()
+{
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    COMPREPLY=( $(compgen -W "$(cat ~/packages/data/all_gogo_projects | while read s; do echo install_$s; done)" -- $cur) )
+}
+
 complete -F _auto_pack uto_pack
-complete -F _auto_pack make
+complete -F _make_go_project make
